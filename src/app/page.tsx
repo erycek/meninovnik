e1244ff7d8e5f61b1cb87a943d7df7105f9c2168
erik <erik.novysedlak@student.tuke.sk>
2023-07-29 16:23:29 +0200

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="relative flex place-items-center">
+
+        <h4 className="text-4xl font-bold text-center text-gray-900 dark:text-gray-100">
+          text
+        </h4>
+
+        <h1></h1>
+      </div>
+    </main>
+  )
+}
