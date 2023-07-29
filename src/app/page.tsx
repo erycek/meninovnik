c26a7c1394bf72391942dbe7fd5a2d3415091cef
erik <erik.novysedlak@student.tuke.sk>
2023-07-29 17:36:41 +0200

    adding firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
-import Image from 'next/image'
+'use client'
+
+import {useEffect, useState} from "react";
 
 export default function Home() {
+  const [name, setName] = useState<string>('');
+  useEffect(() => {
+    fetch('names.json')
+      .then(response => response.json())
+      .then(data => {
+        const month = new Date().getMonth();
+        const day = new Date().getDate();
+        const name = data[month][day];
+        setName(name);
+      });
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center">
-
-        <h4 className="text-4xl font-bold text-center text-gray-900 dark:text-gray-100">
-          text
-        </h4>
-
-        <h1></h1>
+         <div className="sm:max-w-lg">
+          <p className="mt-4 text-xl text-gray-500">dnes má meniny</p>
+          <h1 className="mt-10 text-4xl font-bold tracking-tight text-white-900 sm:text-6xl">{name}</h1>
+        </div>
       </div>
     </main>
   )
